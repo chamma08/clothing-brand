d8b4680f0a8f87ef37c8f2a008488b8694fcc144
Matheesha Dissanayake <SA22524904@my.sliit.lk>
2025-09-29 08:17:55 +0530

    Sign up page functionality done

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function SignIn() {
+    const[form,setForm] = useState({});
   return (
     <div className='min-h-screen flex items-center justify-center bg-gray-100 rounded-2xl'>
       <form className='bg-white p-6 rounded shadow-md w-80'>
